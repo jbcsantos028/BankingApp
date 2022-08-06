a849fe492057ec3b82efc47a2d71a797f3884345
MisterScrooge <shawnhumbert13@gmail.com>
2022-08-06 10:28:41 +0800

    'show-hide-chosen-acc'

--- src/components/ChooseAccount.js
+++ src/components/ChooseAccount.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
+
 const ChooseAccount = props => {
-    // At App.js
+    const [isShown, setIsShown] = useState(true);
     const chosenAccount = props.owners.find(item => item.name.toLowerCase() === props.getName.toLowerCase());
-    // const [showProfile, setShowProfile] = useState('');
     const searchProfile = (e) => {
         if (props.getName !== '') {
             e.preventDefault();
+            setIsShown(false);
             console.log(props.getName)
             props.setWhoseAccount(chosenAccount);
             props.setShowProfile(`Name: ${props.getName.toUpperCase()}  Balance: ${chosenAccount.balance}`);
@@ -13,12 +15,19 @@ const ChooseAccount = props => {
 
     return (
         <div>
-            <form>
-                <label>Enter name of account: </label>
-                <input type="text" value={props.getName} onInput={e => props.setGetName(e.target.value)} />
-                <button onSubmit={searchProfile} onClick={searchProfile}>Submit</button>
-            </form>
-            <div>{props.showProfile}</div>
+            {
+                isShown ?
+                <form>
+                    <label>Enter name of account: </label>
+                    <input type="text" value={props.getName} onInput={e => props.setGetName(e.target.value)} />
+                    <button onSubmit={searchProfile} onClick={searchProfile}>Submit</button>
+                </form>
+                :
+                <div>
+                    <div>{props.showProfile}</div>
+                    <button onClick={() => setIsShown(true)}>Change Account</button>
+                </div>
+        }
         </div>
         
     )
